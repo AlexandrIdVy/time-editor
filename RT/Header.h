c0baba63e5512c8acb6419fcaf0776884f0081e7
Alexandr Idgenov <cop2100@yandex.ru>
2022-08-12 22:19:05 +0700

    Update Header.h

--- RT/Header.h
+++ RT/Header.h
@@ -3,16 +3,16 @@
 
 std::string fileName;
 
-// 1-й канал
+// 1-Р№ РєР°РЅР°Р»
 const std::string fileNamePolikrR5 = "";
 const std::string fileNamePolikrR6 = "";
 const std::string fileNamePolikrR9 = "";
-// 2-й канал
+// 2-Р№ РєР°РЅР°Р»
 const std::string fileNameAZOR1 = "";
 const std::string fileNameAZOR2 = "";
 const std::string fileNameAZOR3 = "";
 const std::string fileNameAZOR4 = "";
-// 3-й канал
+// 3-Р№ РєР°РЅР°Р»
 	//	
 const std::string fileNameNitridR1 = "";
 const std::string fileNameNitridR3 = "";	
@@ -20,33 +20,33 @@ const std::string fileNameNitridR4 = "";
 	// 
 const std::string fileNameNitridR5 = "";
 
-// параметры для посика строки со временем в RTForm.h
+// РїР°СЂР°РјРµС‚СЂС‹ РґР»СЏ РїРѕСЃРёРєР° СЃС‚СЂРѕРєРё СЃРѕ РІСЂРµРјРµРЅРµРј РІ RTForm.h
 int nSTSHPolikr = 16;
 int nSTSHAZO = 14;
-// 3-й канал
+// 3-Р№ РєР°РЅР°Р»
 	// 
 int nSTSHFSS = 15;
 	// 
 int nSTSHNitrid = 16;
 
-// параметры для посика и замены строк в ReplaceRT.h
+// РїР°СЂР°РјРµС‚СЂС‹ РґР»СЏ РїРѕСЃРёРєР° Рё Р·Р°РјРµРЅС‹ СЃС‚СЂРѕРє РІ ReplaceRT.h
 std::string version;
-// 1-й канал
+// 1-Р№ РєР°РЅР°Р»
 std::string versionPolikr = "";
-// 2-й канал
+// 2-Р№ РєР°РЅР°Р»
 std::string versionAZO = "";
-// 3-й канал
+// 3-Р№ РєР°РЅР°Р»
 std::string versionNitrid = "";
 
 int nSTS;
 int nSTR;
-// 1-й канал
+// 1-Р№ РєР°РЅР°Р»
 int nSTSPolikr = 16;
 int nSTRPolikr = 15;
-// 2-й канал
+// 2-Р№ РєР°РЅР°Р»
 int nSTSAZO = 14;
 int nSTRAZO = 13;
-// 3-й канал
+// 3-Р№ РєР°РЅР°Р»
 	// 
 int nSTSFSS = 15;
 int nSTRFSS = 14;
